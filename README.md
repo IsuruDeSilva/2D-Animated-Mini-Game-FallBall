d8669bd8bc646a9d69c77226e19039fcbfe673b4
Isuru de Silva <isurudesilva09@gmail.com>
2023-03-22 08:47:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 2D-Animated-Mini-Game-FallBall
+ Unity based 2D game designed for a university project.
---
+++ VC/3D game1345/Library/Temp/ScriptUpdater/403823980/Assets/scripts/PlayerJump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerJump : MonoBehaviour 
+{
+	public Transform groundCheck;
+     	bool grounded = false;
+     	public int jumpSpeed;     
+
+     	void Update () 
+     	{
+         		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1<< LayerMask.NameToLayer("Ground"));
+         		if ((Input.GetKeyDown("space"))&&(grounded))
+         		{
+             			GetComponent<Rigidbody2D>().AddForce(transform.up*jumpSpeed);
+             			grounded = false; //so you can jump only once
+         		}
+                             
+     	}
+}
